bd7eef966b48f9e66104cfcb0c28ed258d13bbba
Jonas Sieff~ <jonas.sieff@edu.fh-joanneum.at>
2025-11-04 13:21:40 +0100

    added tests

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '**/tests/**/*.test.js',
+    '**/src/**/*.test.js'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html']
+};
